3e66c7ab07f39c7a0151d6ef7ca953606a734635
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-05-20 18:13:02 +0300

    Create winCheck function

---
+++ script.js
@@ -0,0 +1,48 @@
+//ZHEKA - PENIS
+
+const Game = function(){
+
+}
+
+const Player = function(role, sign){
+
+}
+
+function createPlayer(role, sign){
+    return {role, sign};
+}
+
+const GameBoard = function(){
+
+}
+
+function createGameboard(){
+    return [[], [], []];
+}
+
+const arr = [['o', 'o', 'o'], ['x', 'o', 'x'], ['o', 'x', 'x']]
+
+function winCheck(gameBoard){
+   for(element of gameBoard){
+        if(element[0] === element[1] && element[1] === element[2]){
+            return element[0];
+        }
+    };
+    if(gameBoard[0][0] === gameBoard[1][1] &&  gameBoard[1][1] === gameBoard[2][2]){
+        return gameBoard[0][0];
+    }
+    for(let i = 0; i < 2; i++){
+        if(gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]){
+            return gameBoard[0][i];
+        }
+    }
+    return 'draw';
+}
+
+
+function arrCheck(gameBoard){
+    if(gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][1] === gameBoard[0][2]){
+        return gameBoard[0][0];
+    }
+}
+console.log(winCheck(arr));
