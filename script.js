d7fa0ce7cf11aebf0725bf5e794b818b5a9bce48
yurkevichkirill <kirill.iurkievich@mail.ru>
2025-05-22 19:43:59 +0300

    Full logic without styles

--- script.js
+++ script.js
@@ -14,17 +14,18 @@ function chooseRoleDOM(){
     btns.appendChild(changeSign);
 
     continueBtn.addEventListener("click", () => {
-        player1.sign = 'x';
-        player2.sign = 'o';
-        console.log(player1.sign);
-        console.log(player2.sign);
+        if(!isGameStart()){
+            player1.sign = 'x';
+            player2.sign = 'o';
+        }
     })
 
     changeSign.addEventListener('click', () => {
-        player1.sign = 'o';
-        player2.sign = 'x';
-        console.log(player1.sign);
-        console.log(player2.sign);
+        if(!isGameStart()){
+            player1.sign = 'o';
+            player2.sign = 'x';
+            computerMove();
+        }
     })
 }
 
@@ -54,14 +55,20 @@ const gameBoardDOM = function(){
             boardItem.id = `el${i}${j}`;
             boardItem.className = 'boardItem';
             boardItem.addEventListener("click", () => {
-                if(!isBoardFull()){
+                if(!isBoardFull() && !finish){
                     const row = boardItem.id[2];
                     const col = boardItem.id[3];
                     if(gameBoard[row][col] === undefined){
                         gameBoard[row][col] = player1.sign;
                         boardItem.textContent = player1.sign;
+                        if(winCheck() != undefined){
+                            finish = true;
+                            console.log(`${winCheck()} wins`);
+                        }
+                        else{
+                            computerMove();
+                        }
                     }
-                    computerMove();
                 }                
             });
             boardDOM.appendChild(boardItem);
@@ -81,11 +88,13 @@ function computerMove(){
         if(gameBoard[row][col] === undefined){
             break;
         }
-    }        
-
+    }     
     gameBoard[row][col] = player2.sign;
-    document.querySelector(`#el${row}${col}`).textContent = player2.sign;
-
+    document.querySelector(`#el${row}${col}`).textContent = player2.sign;   
+    if(winCheck() != undefined){
+        finish = true;
+        console.log(`${winCheck()} wins`);
+    }
 }
 
 function createGameBoard(){
@@ -94,21 +103,26 @@ function createGameBoard(){
 
 const arr = [['o', 'o', 'o'], ['x', 'o', 'x'], ['o', 'x', 'x']]
 
-function winCheck(gameBoard){
+function winCheck(){
    for(element of gameBoard){
         if(element[0] === element[1] && element[1] === element[2]){
             return element[0];
         }
     };
-    if(gameBoard[0][0] === gameBoard[1][1] &&  gameBoard[1][1] === gameBoard[2][2]){
-        return gameBoard[0][0];
+    if(gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2] ||
+       gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]
+    ){
+        return gameBoard[1][1];
     }
     for(let i = 0; i < 2; i++){
         if(gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]){
             return gameBoard[0][i];
         }
     }
-    return 'draw';
+    if(isBoardFull()){
+        return 'draw';
+    }
+    return undefined;
 }
 
 function isBoardFull(){
@@ -126,13 +140,18 @@ function isBoardFull(){
     return false;
 }
 
-function game(){
-    if(player2.sign === 'x'){
-        computerMove();
+function isGameStart(){
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(gameBoard[i][j] != undefined){
+                return true;
+            }
+        }
     }
-    
+    return false;
 }
 
+let finish = false;
 const player1 = createPlayer('user', 'x');
 const player2 = createBot(player1.sign);
 
